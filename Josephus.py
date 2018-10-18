502b57cb30c9eb19d35001de0bc34f27e4f29256
3170116 <41377852+3170116@users.noreply.github.com>
2018-10-18 22:16:00 +0300

    Update Josephus.py

--- Josephus.py
+++ Josephus.py
@@ -2,13 +2,9 @@ def josephus(aList,start,step):
     if len(aList) == 1:
         return aList[0]
     else:
-        if start+step < len(aList):
-            del aList[start+step]
-            return josephus(aList,start+step-1,step)
+        position = (start+step)%len(aList)
+        del aList[position]
+        if position == 0:
+            return josephus(aList,len(aList)-1,step)
         else:
-            position = ((start+step)-(len(aList)))%len(aList)
-            del aList[position]
-            if position == 0:
-                return josephus(aList,len(aList)-1,step)
-            else:
-                return josephus(aList,position - 1,step)
+            return josephus(aList,position - 1,step)
