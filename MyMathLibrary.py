20b1e3d54478400a8aa750d4f85db9aa20ddca59
3170116 <41377852+3170116@users.noreply.github.com>
2018-10-27 16:02:32 +0300

    Create MyMathLibrary.py

---
+++ MyMathLibrary.py
@@ -0,0 +1,43 @@
+def BolzanoMethod(f, a, b, e):
+    c = a + (b-a)/2.0
+    if b - c <= e or f(c) == 0:
+        return c
+    else:
+        if sign(f(a))*sign(f(c)) < 0:
+            return BolzanoMethod(f,a,c,e)
+        else:
+            return BolzanoMethod(f,c,b,e)
+
+def sign(number):
+    if number < 0:
+        return -1
+    else:
+        return 1
+
+
+def sqrt(number):
+    def f(x):
+        return x**2 - number
+    return BolzanoMethod(f,0,number,10**(-5))
+
+def det(a):
+    if len(a) == 2:
+        return a[0][0]*a[1][1] - a[1][0]*a[0][1]
+    else:
+        d = 0
+        for times in range(len(a)):
+            d += ((-1)**times)*a[0][times]*det(__sub(a,times))
+        return d
+
+
+def __sub(a,j):
+    b = []
+    for i in range(len(a)-1):
+        arr = []
+        for k in range(len(a)):
+            arr.append(a[i+1][k])
+        b.append(arr)
+    size = len(b)
+    for times in range(size):
+        del b[times][j]
+    return b
