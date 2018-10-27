cb2fdb0133f13485cac8076a988905b547176fdb
3170116 <41377852+3170116@users.noreply.github.com>
2018-10-27 16:05:31 +0300

    Update MyMathLibrary.py

--- MyMathLibrary.py
+++ MyMathLibrary.py
@@ -41,3 +41,14 @@ def __sub(a,j):
     for times in range(size):
         del b[times][j]
     return b
+
+def josephus(aList,start,step):
+    if len(aList) == 1:
+        return aList[0]
+    else:
+        position = (start+step)%len(aList)
+        del aList[position]
+        if position == 0:
+            return josephus(aList,len(aList)-1,step)
+        else:
+            return josephus(aList,position - 1,step)
