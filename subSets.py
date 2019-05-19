3b77d86c8014c6f0d88ec11bd24c7f26c619e84b
3170116 <41377852+3170116@users.noreply.github.com>
2019-05-20 01:48:09 +0300

    Add files via upload

---
+++ subSets.py
@@ -0,0 +1,18 @@
+def subSets(items):
+    blocks = [items]
+    stack = []
+    while len(blocks) > 0:
+        block = blocks[0]
+        del blocks[0]
+        if len(block) == 0:
+            break
+        stack.append(block)
+        for i in range(len(block)):
+            new_block = []
+            for j in range(len(block)):
+                if i != j:
+                    new_block.append(block[j])
+            if new_block not in blocks:
+                blocks.append(new_block)
+    while len(stack) > 0:
+        print(stack.pop())
